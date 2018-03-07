f2d531c9b35702f94e24a5d27bb550af7eced482
joshsalvi <jsalvi@rockefeller.edu>
2018-03-06 22:41:35 -0500

    update

--- testrun_vatic_annot.m
+++ testrun_vatic_annot.m
@@ -1,16 +1,28 @@
 annotationsfile = '/Users/joshsalvi/GitHub/vatic-docker/data/output.mat';
 
 load(annotationsfile)
-clear a
+clear a label frames
 
+disp('1')
+q=1;
 for j = 1:length(annotations)
-    a{j} = struct2array(cell2mat(annotations{j}.attributes));
-    frames(j) = annotations{j}.frame;
+    try
+    b = (cell2mat(annotations{j}.attributes));
+    a{q} = struct2array(b);a{q} = a{q}(isstrprop(a{q},'alpha'));
+    label{q} = annotations{j}.label(annotations{j}.label~=' ');
+    frames(q) = annotations{j}.frame;
+    q=q+1;
+    end
 end
-header = unique(a);
-annot_vals = zeros(length(header),frames(end));
+activities = unique(a);
+animals = unique(label);
 
+annot_vals{1} = zeros(length(activities),frames(end));annot_vals{2}=annot_vals{1};
+
+disp('2')
 for j = 1:length(a)
-    q = find(cell2mat(cellfun(@(x) isempty(strfind(x,a{j}))==0,header,'UniformOutput',0))==1);
-    annot_vals(q,frames(j)) = 1;
+    q = find(cell2mat(cellfun(@(x) isempty(strfind(x,a{j}))==0,activities,'UniformOutput',0))==1);
+    r = find(cell2mat(cellfun(@(x) isempty(strfind(x,label{j}))==0,animals,'UniformOutput',0))==1);
+    annot_vals{r}(q,frames(j)) = 1;
 end
+
